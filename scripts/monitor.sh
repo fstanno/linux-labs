d4bec9edb3dc7846b28b8cf1d624a75e860f7eda
Fabiana <fabiana.tanno98@gmail.com>
2026-04-17 14:54:28 -0300

    refatora monitor.sh com funções e melhor organização

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,21 +1,56 @@
+
 #!/bin/bash
 
-LOG_DIR=../logs
-LOG_FILE=$LOG_DIR/system.log
+# ==============================
+# Configurações
+# ==============================
+LOG_DIR="../logs"
+LOG_FILE="$LOG_DIR/system.log"
 
-mkdir -p $LOG_DIR
+# ==============================
+# Funções
+# ==============================
 
-echo "==== $(date) ====" >> $LOG_FILE
-echo "CPU:" >> $LOG_FILE
-top -bn1 | grep "Cpu(s)" >> $LOG_FILE
+create_log_dir() {
+  if [ ! -d "$LOG_DIR" ]; then
+    mkdir -p "$LOG_DIR"
+  fi
+}
 
-echo "MEMORY: " >> $LOG_FILE
-free -h >> $LOG_FILE
+log_header() {
+  echo "=================================" >> "$LOG_FILE"
+  echo "DATE: $(date)" >> "$LOG_FILE"
+  echo "HOST: $(hostname)" >> "$LOG_FILE"
+  echo "=================================" >> "$LOG_FILE"
+}
 
-echo "" >> $LOG_FILE
+log_cpu() {
+  echo "CPU:" >> "$LOG_FILE"
+  top -bn1 | grep "Cpu(s)" >> "$LOG_FILE"
+  echo "" >> "$LOG_FILE"
+}
 
+log_memory() {
+  echo "MEMORY:" >> "$LOG_FILE"
+  free -h >> "$LOG_FILE"
+  echo "" >> "$LOG_FILE"
+}
 
+log_disk() {
+  echo "DISK:" >> "$LOG_FILE"
+  df -h >> "$LOG_FILE"
+  echo "" >> "$LOG_FILE"
+}
 
+# ==============================
+# Execução
+# ==============================
+
+create_log_dir
+log_header
+log_cpu
+log_memory
+log_disk
 
 
 
